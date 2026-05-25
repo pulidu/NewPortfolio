8bc2e166d56aea998f83f177ff92d05e8f435db2
pulidu <godagepulindu@gmail.com>
2026-05-25 20:01:42 +0530

    third commit update about

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  return {
-    plugins: [react()],
-    // 'build' එකකදී පමණක් repository name එක base path එක ලෙස ගනී
-    base: command === 'build' ? '/New_Portfolio/' : '/',
-  }
+export default defineConfig({
+  plugins: [react()],
+
+  base: process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'development' ? '/' : '/',
 })

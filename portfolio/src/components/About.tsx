c3480610117b55be450ff9c7a921c83dedbd6b8e
pulidu <godagepulindu@gmail.com>
2026-09-11 01:47:50 +0530

    21st commit - update about page image path

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -199,7 +199,7 @@ const AboutSection = () => {
               </div>
 
               <img
-                src="/Images/About.jpeg"
+                src="/Images/AboutPage.jpeg"
                 alt="Profile photo of Pulindu Godage"
                 className="relative z-10 w-full h-auto max-h-[350px] md:max-h-[450px] object-cover rounded-2xl sm:rounded-3xl shadow-2xl"
               />
